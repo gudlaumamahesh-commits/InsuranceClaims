d9513580203b05283b7cab05f7cc7b477fabae39
Gudla Umamahesh <2475924@cognizant.com>
2026-04-17 10:43:36 +0530

    removed Comment

--- Repositories/AssessmentRepository.cs
+++ Repositories/AssessmentRepository.cs
@@ -18,7 +18,6 @@ namespace InsuranceClaims.Repositories
                 .Where(sa => sa.SurveyorId == surveyorId)
                 .ToListAsync();
 
-        // addedComment
         public async Task AddAsync(Assessment assessment)
         {
             await _db.Assessments.AddAsync(assessment);
